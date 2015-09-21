7a10cb4739cdad0a4ac846e80388818d2436cacd
Augustin Krug <a.krug@ackr.net>
2015-09-21 15:31:28 +0200

    080

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -5,7 +5,6 @@ Created on Mon Sep 21 11:25:08 2015
 @author: A.KRUG
 """
 
-for n in range(97, 122+1):
-    for m in range(97, 122+1):
-        if n != m:
-            print(chr(n)+chr(m))
+for n in range(97, 122):
+    for m in range(n+1, 122+1):
+        print(chr(n)+chr(m))
