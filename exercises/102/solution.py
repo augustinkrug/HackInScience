af62be131103dd45f4dbc228542ff7c33312a4a6
Augustin Krug <a.krug@ackr.net>
2015-09-21 18:20:40 +0200

    102.4

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -18,27 +18,27 @@ velib = [
         'address':
         "52 RUE D'ENGHIEN / ANGLE RUE DU FAUBOURG POISSONIERE - 75010 PARIS",
         'zip': '75010',
-        'number': 10042, 'latitude': 48.87242006305313, 'city': 'PARIS-',
+        'number': 10042, 'latitude': 48.87242006305313, 'city': 'PARIS',
         'name': 'ENGHIEN-',
         'longitude': 2.348395236282807
     },
     {
         'address': '74 BOULEVARD DES BATIGNOLLES - 75008 PARIS-',
         'zip': '75008',
-        'number': 8020, 'latitude': 48.882148945631904, 'city': 'PARIS-',
+        'number': 8020, 'latitude': 48.882148945631904, 'city': 'PARIS',
         'name': 'METRO ROME-',
         'longitude': 2.319860054774211
     },
     {
         'address': '37 RUE CASANOVA - 75001 PARIS-',
         'zip': '75001',
-        'number': 1022, 'latitude': 48.8682170167744, 'city': 'PARIS-',
+        'number': 1022, 'latitude': 48.8682170167744, 'city': 'PARIS',
         'name': 'RUE DE LA PAIX-',
         'longitude': 2.330493511399174
     },
     {
         'address':
-        '139 AVENUE JEAN LOLIVE / MAIL CHARLES DE GAULLE - 93500 PANTIN-',
+        '139 AVENUE JEAN LOLIVE / MAIL CHARLES DE GAULLE - 93500 PANTIN',
         'zip': '93500',
         'number': 35014,
         'latitude': 48.893268664697416,
