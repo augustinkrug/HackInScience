a378db8b2efbd1d8be2bb466cc195ac049c0cc3a
Augustin Krug <a.krug@ackr.net>
2015-09-21 14:05:11 +0200

    019

---
+++ exercises/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:25:08 2015
+
+@author: A.KRUG
+"""
+
+import sys
+
+if len(sys.argv) > 2:
+    print(sys.argv[1]+sys.argv[2])
+else:
+    print("usage: python3 solution.py OP1 OP2")
