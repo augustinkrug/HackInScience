0c1cb6f6be7db19d8a0dae8467c5c64ad3eec8bd
Augustin Krug <a.krug@ackr.net>
2015-09-21 18:18:22 +0200

    102.2

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -9,7 +9,7 @@ velib = [
     {
         'address':
         'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
-        'zip': '93170-',
+        'zip': '93170',
         'number': 31705, 'latitude': 48.8645278209514, 'city': 'BAGNOLET',
         'name': 'CHAMPEAUX (BAGNOLET)-',
         'longitude': 2.416170724425901
@@ -24,14 +24,14 @@ velib = [
     },
     {
         'address': '74 BOULEVARD DES BATIGNOLLES - 75008 PARIS-',
-        'zip': '75008-',
+        'zip': '75008',
         'number': 8020, 'latitude': 48.882148945631904, 'city': 'PARIS-',
         'name': 'METRO ROME-',
         'longitude': 2.319860054774211
     },
     {
         'address': '37 RUE CASANOVA - 75001 PARIS-',
-        'zip': '75001-',
+        'zip': '75001',
         'number': 1022, 'latitude': 48.8682170167744, 'city': 'PARIS-',
         'name': 'RUE DE LA PAIX-',
         'longitude': 2.330493511399174
@@ -39,7 +39,7 @@ velib = [
     {
         'address':
         '139 AVENUE JEAN LOLIVE / MAIL CHARLES DE GAULLE - 93500 PANTIN-',
-        'zip': '93500-',
+        'zip': '93500',
         'number': 35014,
         'latitude': 48.893268664697416,
         'city': 'PANTIN-',
@@ -60,6 +60,6 @@ def check_my_city(city_name):
     else:
         return stations
 
-"""
-print(check_my_city("PARIS-"))
-"""
+
+print(check_my_city("Bagnolet"))
+
