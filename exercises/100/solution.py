8429bbb9e56f8cfb4f20459e895f5c3ca327641d
Augustin Krug <a.krug@ackr.net>
2015-09-21 17:40:19 +0200

    100.2

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -6,12 +6,12 @@ Created on Mon Sep 21 11:25:08 2015
 """
 
 station = {
- 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
- 'number': 31705,
- 'latitude': 48.8645278209514,
- 'name': 'CHAMPEAUX (BAGNOLET)',
- 'longitude': 2.416170724425901
- }
+    'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+    'number': 31705,
+    'latitude': 48.8645278209514,
+    'name': 'CHAMPEAUX (BAGNOLET)',
+    'longitude': 2.416170724425901
+}
 
 print("latitude " + str(station['latitude']))
 print("longitude " + str(station['longitude']))
