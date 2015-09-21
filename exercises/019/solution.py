6ec750a39c773a9827dfca03d5a54ba1a1a54c71
Augustin Krug <a.krug@ackr.net>
2015-09-21 14:19:33 +0200

    019.1

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -8,6 +8,7 @@ Created on Mon Sep 21 11:25:08 2015
 import sys
 
 if len(sys.argv) > 2:
-    print(sys.argv[1]+sys.argv[2])
+    result = int(sys.argv[1])+int(sys.argv[2])
+    print(result)
 else:
     print("usage: python3 solution.py OP1 OP2")
