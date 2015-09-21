84f1e9bda2db38817981d85a626dfff525868db8
Augustin Krug <a.krug@ackr.net>
2015-09-21 15:28:12 +0200

    070

---
+++ exercises/070/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:25:08 2015
+
+@author: A.KRUG
+"""
+
+for n in range(97, 122+1):
+    for m in range(97, 122+1):
+        if n != m:
+            print(chr(n)+chr(m))
