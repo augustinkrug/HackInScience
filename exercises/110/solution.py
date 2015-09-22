9b3a9fb63b830643af3d5689a3ab21873615ed9f
Augustin Krug <a.krug@ackr.net>
2015-09-22 10:14:52 +0200

    110

---
+++ exercises/110/solution.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:25:08 2015
+
+@author: A.KRUG
+"""
+from sys import argv
+
+
+try:
+    if (argv(1) is int and argv(3) is int):
+        if argv(2) == "+":
+            print(argv(1) + argv(3))
+        elif argv(2) == "-":
+            print(argv(1) - argv(3))
+        elif argv(2) == "-":
+            print(argv(1) - argv(3))
+        elif argv(2) == "*":
+            print(argv(1) * argv(3))
+        elif argv(2) == "/":
+            print(argv(1) / argv(3))
+        elif argv(2) == "%":
+            print(argv(1) % argv(3))
+        elif argv(2) == "^":
+            print(argv(1) ^ argv(3))
+        else:
+            print("input error")
+    else:
+        print("input error")
+except:
+    print("input error")
