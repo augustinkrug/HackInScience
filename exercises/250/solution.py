e1c561d4f52cfa51fdc788d5644f5b0099053d1a
Augustin Krug <a.krug@ackr.net>
2015-09-22 16:45:28 +0200

    250.1

--- exercises/250/solution.py
+++ exercises/250/solution.py
@@ -9,10 +9,10 @@ Created on Tue Sep 22 13:33:07 2015
 def draw_n_squares(n):
     line1 = n * "+---" + "+"
     line2 = n * "|   " + "|"
+    squares = ""
     for i in range(1, n + 1):
-        print(line1)
-        print(line2)
-    print(line1)
+        squares = squares + line1 + "\n" + line2 + "\n"
+    return squares + line1
 """
-draw_n_squares(5)
+print(draw_n_squares(5))
 """
---
+++ exercises/260/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 13:33:07 2015
+
+@author: A.KRUG
+"""
+
+
+def euclidean(a, b):
+    
