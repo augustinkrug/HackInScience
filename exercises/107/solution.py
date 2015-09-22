aeea9ad2d4408f3f760471aadfe643ba158e6947
Augustin Krug <a.krug@ackr.net>
2015-09-22 09:51:11 +0200

    107

---
+++ exercises/107/solution.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:25:08 2015
+
+@author: A.KRUG
+"""
+from operator import itemgetter
+
+
+def select_student(my_class, mark):
+    accepted = []
+    rejected = []
+    for i in range(0, len(my_class)):
+        if my_class[i][1] >= mark:
+            accepted.append(my_class[i])
+        else:
+            rejected.append(my_class[i])
+    return {'Accepted': sorted(accepted, reverse=True, key=itemgetter(1)),
+            'Rejected': sorted(rejected, key=itemgetter(1))}
+
+"""
+my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67],
+            ['Ben Ball', 5], ['William Lee', 2]]
+print(select_student(my_class, 20))
+"""
