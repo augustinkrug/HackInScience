ac094c7b0748ca886ce3fc22d5c1c0f4d5c21f1b
Augustin Krug <a.krug@ackr.net>
2015-09-22 16:16:25 +0200

    240

--- exercises/240/solution.py
+++ exercises/240/solution.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 22 15:18:25 2015
+Created on Tue Sep 22 13:33:07 2015
 
 @author: A.KRUG
 """
 
-
-
+output = "1, 2, "
+i = 2
+nb1 = 1
+nb2 = 2
+while i < 10:
+    nb = nb1 + nb2
+    output = output + str(nb) + ", "
+    i = i + 1
+    nb1 = nb2
+    nb2 = nb
+output = output[:-2] + "."
+print(output)
