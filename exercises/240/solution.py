d097c3fc714f8d5d2e017e293c53ca961f453573
Augustin Krug <a.krug@ackr.net>
2015-09-22 16:19:29 +0200

    240

--- exercises/240/solution.py
+++ exercises/240/solution.py
@@ -9,11 +9,14 @@ output = "1, 2, "
 i = 2
 nb1 = 1
 nb2 = 2
+
 while i < 10:
     nb = nb1 + nb2
     output = output + str(nb) + ", "
     i = i + 1
     nb1 = nb2
     nb2 = nb
+
 output = output[:-2] + "."
+
 print(output)
