3f7e7a419ed376aeea7e2f59ea940b0c7f84269d
Augustin Krug <a.krug@ackr.net>
2015-09-22 11:20:45 +0200

    110.3

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -7,27 +7,29 @@ Created on Mon Sep 21 11:25:08 2015
 from sys import argv
 
 
-try:
-    if len(argv) < 4:
-        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
-    elif (argv(1) is int and argv(3) is int):
-        if argv(2) == "+":
-            print(argv(1) + argv(3))
-        elif argv(2) == "-":
-            print(argv(1) - argv(3))
-        elif argv(2) == "-":
-            print(argv(1) - argv(3))
-        elif argv(2) == "*":
-            print(argv(1) * argv(3))
-        elif argv(2) == "/":
-            print(argv(1) / argv(3))
-        elif argv(2) == "%":
-            print(argv(1) % argv(3))
-        elif argv(2) == "^":
-            print(argv(1) ^ argv(3))
-        else:
-            print("input error")
-    else:
-        print("input error")
-except:
+def calc(a, op, b):
+    if op == "+":
+        print(a + b)
+    elif op == "-":
+        print(a - b)
+    elif op == "*":
+        print(a * b)
+    elif op == "/":
+        print(a / b)
+    elif op == "%":
+        print(a % b)
+    elif op == "^":
+        print(a ^ b)
+
+if len(argv) < 4:
+    print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+elif argv[2] not in ["+", "-", "*", "/", "%", "^"]:
     print("input error")
+else:
+    try:
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+        calc(a, op, b)
+    except:
+        print("input error")
