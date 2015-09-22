827e85397e01d78f95fb9856fa270ff7ffbc512d
Augustin Krug <a.krug@ackr.net>
2015-09-22 14:33:19 +0200

    210

---
+++ exercises/210/is_prime.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:25:08 2015
+
+@author: A.KRUG
+"""
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+    return True
---
+++ exercises/210/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 13:33:07 2015
+
+@author: A.KRUG
+"""
+
+from is_prime import is_prime
+
+somme = 0
+
+for i in range(1, 1000):
+    if is_prime(i) == True:
+        somme = somme + i
+
+print(somme)
