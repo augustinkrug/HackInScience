a58c5211e661bbf96996326c597b4eb99c03ab77
Augustin Krug <a.krug@ackr.net>
2015-09-22 18:05:38 +0200

    285

---
+++ exercises/285/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 13:33:07 2015
+
+@author: A.KRUG
+"""
+
+from requests import get
+
+try:
+    r = get('http://mdk.fr/ip')
+except:
+    print("No internet connectivity.")
+
+text = r.text
+for i in range(len(r.text)):
+    if text[i] == "\n":
+        text = text[:i]
+        print(text)
+        break
