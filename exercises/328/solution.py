286fa341fbd5ce26d245eabfd0ba3fcb7e2c9db4
Augustin Krug <a.krug@ackr.net>
2015-09-24 11:55:08 +0200

    329

--- exercises/328/solution.py
+++ exercises/328/solution.py
@@ -7,8 +7,8 @@ Created on Tue Sep 22 13:33:07 2015
 
 
 def mul(l):
-    if len(l) == 1:
-        return l[0]
+    # if len(l) == 1:
+        # return l[0]
     result = 1
     for i in l:
         result = result * i
