b5cad102c61e7bee7c0bd9f4f9c2cef123bf7b27
Augustin Krug <a.krug@ackr.net>
2015-09-24 10:07:36 +0200

    300

--- exercises/300/solution.py
+++ exercises/300/solution.py
@@ -5,15 +5,6 @@ Created on Tue Sep 22 13:33:07 2015
 @author: A.KRUG
 """
 
-from requests import get
+w = open("words.txt")
 
-try:
-    r = get('http://mdk.fr/ip')
-    text = r.text
-    for i in range(len(r.text)):
-        if text[i] == "\n":
-            text = text[:i]
-            print(text)
-            break
-except:
-    print("No internet connectivity.")
+w.read()
