11aed6736f50d1806e1b7973cc02a48bed8d5e79
Augustin Krug <a.krug@ackr.net>
2015-09-24 10:59:24 +0200

    328

---
+++ exercises/328/solution.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 13:33:07 2015
+
+@author: A.KRUG
+"""
+
+
+def mul(l):
+    if len(l) == 1:
+        return l[0]
+    result = 1
+    for i in l:
+        result = result * i
+    return result
+
+"""
+print(mul([2]))
+"""
