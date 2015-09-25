63113eb3c2ad46d924d7deaefa65086a6745498f
Augustin Krug <a.krug@ackr.net>
2015-09-25 12:08:19 +0200

    501.1

--- exercises/501/solution.py
+++ exercises/501/solution.py
@@ -44,4 +44,4 @@ def changes(amout, coins):
         change = sorted(change, reverse=True)
     return count
 
-#print(changes(42, (1, 2, 5, 10, 20, 50, 100, 200, 500)))
+# print(changes(42, (1, 2, 5, 10, 20, 50, 100, 200, 500)))
