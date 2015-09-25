fe50bd8b5e16851c0e650b65a8063442f92fe5d9
Augustin Krug <a.krug@ackr.net>
2015-09-25 17:40:14 +0200

    501.3

--- exercises/501/solution.py
+++ exercises/501/solution.py
@@ -6,42 +6,21 @@ Created on Tue Sep 22 13:33:07 2015
 """
 
 
-def best_change(amout, coins):
-    change = []
-    if amout <= coins[-1]:
-        return None
-    for i in coins:
-        if i < amout:
-            change.append(i)
-            amout = amout - i
-            break
-    while amout != 0:
-        for i in coins:
-            if i <= amout:
-                change.append(i)
-                amout = amout - i
-                break
-    return change
-
-
-def changes(amout, coins):
-    coins = tuple(sorted(coins, reverse=True))
-    count = 1
-    count_init = 0
-    change = [amout]
-    while count_init != count:
-        # print(change)
-        # print(count)
-        count_init = count
-        for i in reversed(range(0, len(change))):
-            X = best_change(change[i], coins)
-            if X is not None:
-                count = count + 1
-                del change[i]
-                for j in X:
-                    change.append(j)
-                break
-        change = sorted(change, reverse=True)
+def changes2(amount, coins_rv):
+    count = 0
+    while coins_rv[0] > amount:
+        del coins_rv[0]
+    maxi = amount // coins_rv[0]
+    for i in reversed(range(0, maxi + 1)):
+        rest = amount - i * coins_rv[0]
+        if rest == 0:
+            count = count + 1
+        else:
+            if len(coins_rv) > 1:
+                count = count + changes2(rest, coins_rv[1:])
     return count
 
-# print(changes(42, (1, 2, 5, 10, 20, 50, 100, 200, 500)))
+
+def changes(amount, coins):
+    coins_rv = sorted(coins, reverse=True)
+    return changes2(amount, coins_rv)
