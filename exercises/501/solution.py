5b9d0416748a69ae260f7e904caf56c7cecb99a1
Augustin Krug <a.krug@ackr.net>
2015-09-25 12:07:09 +0200

    501.1

--- exercises/501/solution.py
+++ exercises/501/solution.py
@@ -5,6 +5,7 @@ Created on Tue Sep 22 13:33:07 2015
 @author: A.KRUG
 """
 
+
 def best_change(amout, coins):
     change = []
     if amout <= coins[-1]:
@@ -22,18 +23,19 @@ def best_change(amout, coins):
                 break
     return change
 
+
 def changes(amout, coins):
-    coins = tuple(sorted(coins, reverse = True))
+    coins = tuple(sorted(coins, reverse=True))
     count = 0
     count_init = -1
     change = [amout]
     while count_init != count:
-        #print(change)
-        #print(count)
+        # print(change)
+        # print(count)
         count_init = count
         for i in reversed(range(0, len(change))):
             X = best_change(change[i], coins)
-            if X != None:
+            if X is not None:
                 count = count + 1
                 del change[i]
                 for j in X:
@@ -42,4 +44,4 @@ def changes(amout, coins):
         change = sorted(change, reverse=True)
     return count
 
-print(changes(42, (1, 2, 5, 10, 20, 50, 100, 200, 500)))
+#print(changes(42, (1, 2, 5, 10, 20, 50, 100, 200, 500)))
